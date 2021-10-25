5cf307c04ba83d78690ff0fdae318fd80809c54f
vladtsap <vldzpp@gmail.com>
2021-10-25 21:37:22 +0300

    send file via fastAPI while developing

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from mimetypes import guess_type
 
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import Response
+from fastapi.responses import Response, FileResponse
 from pathlib import Path
 
 app = FastAPI(
@@ -11,7 +11,7 @@ app = FastAPI(
 
 
 @app.get('/{image_name}')
-async def image_via_header(image_name: str):
+async def get_image(image_name: str):
     """
     Logic: https://github.com/vas3k/i.vas3k.ru/blob/7ff16e05806bbdb211ecdf6fee79eabb39a8b6dc/helpers.py#L70
 
@@ -25,7 +25,11 @@ async def image_via_header(image_name: str):
     if not Path(filepath).is_file():
         raise HTTPException(status_code=404, detail='image not found')
 
-    return Response(
-        media_type=guess_type(filepath)[0],
-        headers={'X-Accel-Redirect': filepath},
+    return FileResponse(
+        path=f'originals/{image_name}',
     )
+
+    # return Response(
+    #     media_type=guess_type(filepath)[0],
+    #     headers={'X-Accel-Redirect': filepath},
+    # )
