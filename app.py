c61271e3dedcbeeb28e49de8b5ecc8464b02e37e
vladtsap <vldzpp@gmail.com>
2021-10-26 00:44:16 +0300

    fix bug with nginx

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from mimetypes import guess_type
-
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import Response
+from fastapi.responses import FileResponse
 from pathlib import Path
 
 from admin import router as admin_router
@@ -16,20 +14,11 @@ app.include_router(admin_router)
 
 @app.get('/{image_name}')
 async def get_image(image_name: str):
-    """
-    Logic: https://github.com/vas3k/i.vas3k.ru/blob/7ff16e05806bbdb211ecdf6fee79eabb39a8b6dc/helpers.py#L70
-
-    Nginx X-Accel Redirect magic.
-    That headers will distribute statics files through nginx instead of python.
-    Description: https://kovyrin.net/2006/11/01/nginx-x-accel-redirect-php-rails/
-    """
-
     filepath = f'originals/{image_name}'
 
     if not Path(filepath).is_file():
         raise HTTPException(status_code=404, detail='image not found')
 
-    return Response(
-        media_type=guess_type(filepath)[0],
-        headers={'X-Accel-Redirect': filepath},
+    return FileResponse(
+        path=filepath,
     )
